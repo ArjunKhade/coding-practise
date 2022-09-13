3b0b3f3eaa4eb97ff20757c6705a90a5af37758a
ArjunKhade <106642798+ArjunKhade@users.noreply.github.com>
2022-09-13 23:44:13 +0530

    spring-boot

---
+++ src/main/java/com/app/entities/Seller.java
@@ -0,0 +1,57 @@
+package com.app.entities;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString(exclude = "productList")
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "seller")
+public class Seller  extends BaseEntity  {
+	
+	@Column(length = 20)
+	private String name;
+
+	// add unique constraint to email
+	@Column(length = 30, unique = true)
+	private String email;
+
+	@Column(length = 20)
+	private String password;
+
+	@Column(length = 20)
+	private String phone;
+
+	@Column(length = 20)
+	private String city;
+
+	@Column(length = 20)
+	private String pin;
+
+	@Column(name = "address", length = 300)
+	private String address;
+
+	@Column(name="gstin" , length=15)
+	 private String gstin;
+	
+	@Column(name="revenue")
+	 private double revenue;
+	
+	@OneToMany(mappedBy = "seller" , fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	 private List<Product> productList;
+}
